a08537e729af05834ca82c0627ad9c477350068e
Igusaya <igusaya2014@gmail.com>
2019-12-30 22:32:18 +0900

    [fix] local apperr fix

--- Pourmee_BackEnd/settings.py
+++ Pourmee_BackEnd/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 try:
-    from.local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
 
