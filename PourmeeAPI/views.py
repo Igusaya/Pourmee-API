69a7aab39b6c3dc37ab10a4ddd3c76dbf25f172b
Igusaya <igusaya2014@gmail.com>
2019-12-31 00:48:53 +0900

    [add] Add card model

--- PourmeeAPI/views.py
+++ PourmeeAPI/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from PourmeeAPI.models import Card
+from PourmeeAPI.serializers import CardSerializer
 
-# Create your views here.
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class CardList(APIView):
+    """
+    List all Card, or create a new card.
+    """
+    def get(self, request, format=None):
+        card = Card.objects.all()
+        serializer = CardSerializer(card, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
